c40a08b93435380935a23584f6bbeac7a10d5a24
ashok34589 <ashok34589@gmail.com>
2020-02-13 17:38:31 +0530

    Add project files.

---
+++ ConsoleApp1/HarmonicNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class HarmonicNumber
+    {
+        public HarmonicNumber()
+        {
+            Console.WriteLine("enter the nth value for Harmonic Number:");
+            int h = (Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("the nth harmonic value of "+h+" is "+Harmonic(h));
+        }
+        public double Harmonic(int n)
+        {
+            double value = 0, i = 1;
+            while(i<=n)
+            {
+                value +=( 1 / i++);
+            }
+            return value;
+        }
+    }
+}
---
+++ ConsoleApp1/LeapYear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class LeapYear
+    {
+        public LeapYear()
+        {
+            Console.WriteLine("enter the year to check..:");
+            int year = (Convert.ToInt32(Console.ReadLine()));
+            IsLeap(year);
+        }
+        public void IsLeap(int n)
+        {
+            if ((n % 400 == 0) || (n % 100 != 0 && n % 4 == 0))
+                Console.WriteLine(n + " is Leap year...!");
+            else
+                Console.WriteLine(n+" is not a Leap year.....!");
+        }
+    }
+}
