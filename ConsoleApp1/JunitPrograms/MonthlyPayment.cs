0b812ef961eb215d803586801611cfda8386275a
ashok34589 <ashok34589@gmail.com>
2020-02-15 18:52:49 +0530

    [ashok]refactor. changed the directory name

---
+++ ConsoleApp1/JunitPrograms/MonthlyPayment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MonthlyPayment
+    {
+        public MonthlyPayment()
+        {
+            Utility.Print("enter the Principal amount : ");
+            int p = Utility.ReadInt();
+            Utility.Print("enter no. of Years for PayOff : ");
+            int y = Utility.ReadInt();
+            Utility.Print("enter the interest rate in Percentage : ");
+            int r = Utility.ReadInt();
+            Console.WriteLine(Payment(p,y,r));
+        }
+        public int Payment(int p,int y,int r)
+        {
+            double R = r / (12 * 100);
+            int n = 12 * y;
+            return (int)((p*R)/(1-Math.Pow((1+R),-n)));
+
+        }
+    }
+}
---
+++ ConsoleApp1/JunitPrograms/ToBinary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ToBinary
+    {
+        public ToBinary()
+        {
+            Console.Write("enter the number for its binary form: ");
+            Console.WriteLine(Binary(Utility.ReadInt()));
+        }
+        public static string Binary(int n)
+        {
+            string st = "";
+            while(n>0)
+            {
+                st = n % 2 + st;
+                n /= 2;
+            }
+            return st;
+        }
+    }
+}
