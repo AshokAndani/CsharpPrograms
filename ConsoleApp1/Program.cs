04834b70b7f6460cfab5868e8eb348dc87158363
ashok34589 <ashok34589@gmail.com>
2020-02-15 15:58:05 +0530

    [ashok]refactor. Modified Driver Program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    class Program //driver code
     {
         static void Main(string[] args)
         {
@@ -10,64 +10,26 @@ namespace ConsoleApp1
             int number = Utility.ReadInt();
             switch (number)
             {
-                
-                case 1:
-                    new LeapYear();
-                    break;
-                case 2:
-                    new Factorial();
-                    break;
-                case 3:
-                    new HarmonicNumber();
-                    break;
-                case 4:
-                    new UserInput();
-                    break;
-                case 5:
-                    new FlipCoin();
-                    break;
-                case 6:
-                    new TwoDimensionalArray();
-                    break;
-                case 7:
-                    new PrimeFactors();
-                    break;
-                case 8:
-                    new PowerOf2();
-                    break;
-                case 9:
-                    new TripletsEqaulsToZero();
-                    break;
-                case 10:
-                    new Distance();
-                    break;
-                case 11:
-                    new Quadratic();
-                    break;
-                case 12:
-                    new WindChill();
-                    break;
-                case 13:
-                    new GamblingSimulator();
-                    break;
-                case 15:
-                    new CouponNumberGenerator();
-                    break;
-                case 16:
-                    new StopWatch();
-                    break;
-                case 17:
-                    new TicTacToe();
-                    break;
-                case 18:
-                    new VendingMachine();
-                        break;
-                case 19:
-                    new DayOfWeek();
-                    break;
-                default:
-                    Console.WriteLine("invalid Entry");
-                    break;
+
+                case 1:new LeapYear(); break;
+                case 2:new Factorial(); break;
+                case 3:new HarmonicNumber(); break;
+                case 4: new UserInput();break;
+                case 5:new FlipCoin(); break;
+                case 6:new TwoDimensionalArray(); break;
+                case 7:new PowerOf2(); break;
+                case 8:new TripletsEqaulsToZero(); break;
+                case 9: new DayOfWeek(); break;
+                case 10:new Distance(); break;
+                case 11:new Quadratic(); break;
+                case 12:new WindChill(); break;
+                case 13:new GamblingSimulator(); break;
+                case 15:new CouponNumberGenerator(); break;
+                case 16:new StopWatch(); break;
+                case 17:new TicTacToe(); break;
+                case 18:new VendingMachine(); break;
+                case 19: new TemperatureConversion(); break;
+                default:Console.WriteLine("invalid Entry"); break;
             }
 
         }
