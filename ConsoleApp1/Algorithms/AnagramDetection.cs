483dbc5d0b25b9b7840f6125875d8a5dc1379d9d
ashok34589 <ashok34589@gmail.com>
2020-02-17 18:34:41 +0530

    [ashok]add. added few codes

---
+++ ConsoleApp1/Algorithms/AnagramDetection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class AnagramDetection
+    {
+        public static bool IsAnagram(String word, string word1)
+        {
+            if (word.Length != word1.Length)
+                return false;
+            char[] c1 = word.ToCharArray(); Sorting.BubbleSortChar(c1);
+            char[] c2 = word1.ToCharArray(); Sorting.BubbleSortChar(c2);
+            for(int i=0;i<c1.Length;i++)
+            {
+                if (c1[i] != c2[i])
+                    return false;
+            }
+            return true;
+
+
+        }
+        public static void DriverMethod()
+        {
+            Console.Write("enter the 1st Word: ");
+            string word=Console.ReadLine();
+            Console.Write("enter the 2nd Word: ");
+            string word1 = Console.ReadLine();
+            Console.Write(IsAnagram(word, word1));
+        }
+    }
+}
---
+++ ConsoleApp1/Algorithms/RangeOfPrimeNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class RangeOfPrimeNumbers
+    {
+        public static bool isPrime(int n)
+        {
+            int count = 1, i = 2;
+            while (n / 2 >= i)
+            {
+                if (n % i == 0)
+                { count += 1; }
+                if (count >= 2)
+                { return false; }
+                i++;
+            }
+            return true;
+        }
+        public static int PrimeCount(int s,int l)
+        {
+            int count = 0;
+            for(int i=s;i<=l;i++)
+            {
+                if (isPrime(i))
+                { count++; }
+            }
+            return count;
+        }
+        public static int[] PrimeRange(int s,int l)
+        {
+            int[] arr = new int[PrimeCount(s,l)];
+            int i = 0;
+            while(s<l)
+            {
+                if(isPrime(s))
+                {
+                    arr[i++] = s ;
+                }
+                s++;
+            }
+            return arr;
+
+        }
+        public static void DriverMethod()
+        {
+            Console.Write("enter the min range : ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the Max range : ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] a = PrimeRange(min, max);
+            Console.WriteLine("the Prime numbers in the range are: ");
+            foreach(int n in a)
+            {
+                Console.Write(n + " ");
+            }
+            
+        }
+    }
+}
