8cc48894b9ca678a2d4b13e2164296dfc3556823
ashok34589 <ashok34589@gmail.com>
2020-02-20 13:31:37 +0530

    [ashok]add. added ReadFromFile

---
+++ ConsoleApp1/DataStructures/UnOrderedList/ReadFromFile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ConsoleApp1.DataStructures.UnOrderedList;
+
+namespace ConsoleApp1.DataStructures.UnOrderedList
+{
+    class ReadFromFile
+    {
+        public static void DriverMethod()
+        {
+            
+            Console.WriteLine("enter the file fileName: ");
+            string FilePath = Path.GetFullPath(Console.ReadLine());
+            String[] array = File.ReadAllText(FilePath).Split(" ");
+            List list=ArrayToList(array);
+            Console.Write("enter the word : ");
+            string word = Console.ReadLine();
+            Console.WriteLine("Before....Processing....................");
+            String content = File.ReadAllText(FilePath);
+            Console.WriteLine(content);
+            if (list.Search(word))
+            {
+                Console.WriteLine(word + " is found in the file so removing it..");
+                list.Remove(word);
+                
+            }
+            else
+            {
+                Console.WriteLine(word + " is not found so adding the it.... ");
+                list.Append(word);
+            }
+            Console.WriteLine("after Processing.....................");
+            Console.WriteLine(list);
+            String[] arr = ListToArray(list);
+            File.WriteAllText(FilePath,string.Join(" ",arr));
+            Console.WriteLine("This is from final File:....");
+            Console.WriteLine(File.ReadAllText(FilePath));
+        }
+        public static List ArrayToList(string[] arr)
+        {
+            List list = new List();
+            foreach (string content in arr)
+                list.Append(content);
+            return list;
+        }
+        public static string[] ListToArray(List list)
+        {
+            String[] arr= new string[list.Size()];
+            for(int i=0;i<list.Size();i++)
+            {
+                arr[i] = (String)list.peek(i);
+            }
+            return arr;
+        }
+    }
+}
