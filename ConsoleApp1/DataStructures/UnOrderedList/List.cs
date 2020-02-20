44cc9188ef94325b4ac618bfe28611c7f7d268c3
ashok34589 <ashok34589@gmail.com>
2020-02-20 13:30:51 +0530

    [ashok]add. added List

---
+++ ConsoleApp1/DataStructures/UnOrderedList/List.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.DataStructures.UnOrderedList
+{
+    class List
+    {
+        public class Node
+        {
+            public Object data;
+            public Node next;
+            public Node(Object data)
+            {
+                this.data = data;
+            }
+        }
+        public Node head;
+        //correct
+        public bool Append(Object data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return true;
+            }
+
+            Node t = head;
+            while (t.next != null)
+            {
+                t = t.next;
+            }
+            t.next = n;
+            return true;
+        }
+        //correct
+        public Object Pop()
+        {
+            if (head == null)
+            {
+                throw new NullReferenceException("List is Empty");
+            }
+            Node t = head, p = head;
+            while (t.next != null)
+            {
+                p = t;
+                t = t.next;
+            }
+            Object obj = t.data;
+            p.next = null;
+            return obj;
+        }
+        public object peek(int ind)
+        {
+            if (head == null)
+                return null;
+            if (ind == 0)
+                return head.data;
+            Node t = head;
+            while(ind>0)
+            {
+                ind--;
+                t = t.next;
+            }
+            if (ind == 0)
+                return t.data;
+            return null;
+        }
+        //correct
+        public Object peek()
+        {
+            if (head == null)
+                throw new NullReferenceException("empty List");
+            Node t = head, p = head;
+            while (t.next != null)
+            {
+                p = t;
+                t = t.next;
+            }
+            Object obj = t.data;
+            return obj;
+        }
+        //correct
+        public bool Search(Object data)
+        {
+            if (head == null)
+                throw new NullReferenceException("empty List");
+            Node t = head;
+            while (t != null)
+            {
+                if (t.data.Equals(data))
+                    return true;
+                t = t.next;
+            }
+            return false;
+        }
+        //correct
+        public bool IsEmpty()
+        {
+            if (head == null)
+                return true;
+            return false;
+        }
+        //correct
+        public int Size()
+        {
+            if (head == null)
+                return 0;
+            Node t = head; int count = 0;
+            while (t != null)
+            {
+                count++;
+                t = t.next;
+            }
+            return count;
+        }
+        //correct
+        public bool Add(Object data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return true;
+            }
+            n.next = head;
+            head = n;
+            return true;
+        }
+        //correct
+        public override String ToString()
+        {
+            if (head == null)
+                return null;
+            Node t = head;
+            string s = "";
+            while (t != null)
+            {
+                s = s + t.data+" ";
+                t = t.next;
+            }
+            return s;
+        }
+        //correct
+        public int Index(Object data)
+        {
+            int count = -1;
+            if (head == null)
+                throw new NullReferenceException("Empty List");
+            Node t = head;
+            while (t != null)
+            {
+                count++;
+                if (t.data.Equals(data))
+                    break;
+                t = t.next;
+
+            }
+            return count;
+        }
+        public bool Insert(int ind, Object data)
+        {
+            Node n = new Node(data);
+            if (ind == 0)
+            {
+                n.next = head.next;
+                head = n;
+                return true;
+            }
+            Node t = head, pre = null;
+            while (ind > 0 && t != null)
+            {
+                ind--;
+                pre = t;
+                t = t.next;
+            }
+            if (ind == 0)
+            {
+                pre.next = n;
+                n.next = t;
+                return true;
+            }
+            throw new NullReferenceException("index is not in range");
+        }
+        //correct
+        public bool Remove(Object data)
+        {
+            if (head == null)
+                throw new NullReferenceException("list is empty");
+            if (head.next == null)
+                head = null;
+            Node t = head, pre = null;
+            while (t != null)
+            {
+                if (t.data.Equals(data))
+                {
+                    pre.next = t.next;
+                    return true;
+                }
+                pre = t;
+                t = t.next;
+            }
+            return false;
+        }
+        public Object Pop(int ind)
+        {
+            Object obj;
+            if (ind == 0)
+            {
+                obj = head.data;
+                head = head.next;
+                return obj;
+            }
+            Node t = head, pre = null;
+            while(ind>0 && t!=null)
+            {
+                ind--;
+                pre = t;
+                t = t.next;
+            }
+             obj= t.data;
+
+            if (ind == 0)
+            {
+                pre.next = t.next;
+                return obj;
+            }
+            throw new NullReferenceException("index is not range");
+            
+            
+        }
+    }
+}
