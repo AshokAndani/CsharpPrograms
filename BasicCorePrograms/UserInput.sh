896f98de9c925abb34f17e4c4886046b4095484c
ashok34589 <ashok34589@gmail.com>
2020-02-26 17:57:13 +0530

    [ashok]refactor. deleted some files

--- BasicCorePrograms/UserInput.sh
+++
@@ -1,7 +0,0 @@
-read -p 'enter your name : ' name
-if [ ${#name} -ge 3 ]
-then
-echo "Hello $name , How are you...!"
-else 
-echo enter a valid name...!
-fi
--- FuntionalPrograms/DistanceProgram.sh
+++
@@ -1,4 +0,0 @@
-read -p 'enter the x distance: ' x
-read -p 'enter the y distance: ' y
-num=$( awk -v a=$x -v b=$y 'BEGIN { print (((a*a)+(b*b))**0.5) }' )
-echo "the distance from (origin)(0,0) is $num"
