fa5211c0b9c0f6ca0f31a8aeeb5ee2c99a97cb40
ashok34589 <ashok34589@gmail.com>
2020-02-27 11:13:49 +0530

    [ashok]refactor. added Display 2D array method to Calender program

--- DataStructures/Calender.cs
+++ DataStructures/Calender.cs
@@ -7,7 +7,18 @@ namespace DataStructures
     class Calender
     {
         
-        
+        public void DisplayArray(int[,] a)
+        {
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++)
+                {
+                      if(a[i,j]!=0 && j!=0)  Console.Write(a[i,j]+" ");
+                    if (i!=1 && j == 0) Console.Write(a[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
 
     }
 }
