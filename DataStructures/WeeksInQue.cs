180bd3098056f17ced721d2690fb9382fca73461
ashok34589 <ashok34589@gmail.com>
2020-02-27 11:56:43 +0530

    [ashok]add. added WeeksInQue.cs code

---
+++ DataStructures/WeeksInQue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class WeeksInQue
+    {
+
+        public static void DriverMethod()
+        {
+            Calender calender = new Calender();
+            
+        }
+    }
+}
