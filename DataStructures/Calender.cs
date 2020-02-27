3b2b52728df9892fd2e1005698070b2d4b1d83af
ashok34589 <ashok34589@gmail.com>
2020-02-27 11:45:37 +0530

    [ashok]refactor. modified calender() in calender.cs

--- DataStructures/Calender.cs
+++ DataStructures/Calender.cs
@@ -20,13 +20,12 @@ namespace DataStructures
             int d0 = (d + x + 31 * m0 / 12) % 7;
             return d0;
         }
-  public int[,] calender(int m,int y)
+ public int[,] calender(int m,int y)
         {
-            String[] months = {"","January","February","March","April","May","June",
-                "July","August","September","October","November","December"};
             int[] days_in_month = {0,31,28,31,30,31,30,31,31,30,31,30,31};
             if (IsLeapYear(y))
                 days_in_month[2] = 29;
+            
             //filling the array with the dates
             int[,] res= new int[2,days_in_month[m]+1];
             for(int i=0;i<2;i++)
@@ -36,26 +35,9 @@ namespace DataStructures
                     if(i==0 && j<=6)res[i, j] = j;
                     if (i == 1) res[i, j] = j;
                     
-                }      }
-            int d = DayOfWeek(1, m, y);
-            char[] week = { 'S', 'M', 'T', 'W', 'T', 'F', 'S' };
-            //Calender Display Starts from here
-            Console.WriteLine(months[m]+" "+y);
-            for(int i=0;i<week.Length;i++)
-            {
-                Console.Write(week[i]+"  ");
-            }
-            Console.WriteLine();
-            for (int i=0;i<d;i++)
-            {
-                Console.Write("{0}  ",' ');
-            }
-
-            for(int i=1;i<=days_in_month[m];i++)
-            {
-                Console.Write("{0,2} ",i);
-                if ((i+d) % 7 == 0) Console.WriteLine();
-            }            return res;
+                }
+            }            
+            return res;
         }
 public static void DriverMethod()
         {
