ba4974a718c329e9e659ece69ab5e69274f17226
ashok34589 <ashok34589@gmail.com>
2020-02-27 14:55:06 +0530

    [ashok]refactor created an Week object

--- DataStructures/WeeksInQue.cs
+++ DataStructures/WeeksInQue.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using DataStructures.Queue;
+using DataStructures.UnOrderedList;
 
 namespace DataStructures
 {
@@ -9,7 +10,45 @@ namespace DataStructures
 
         public static void DriverMethod()
         {
+            WeeksInQue w = new WeeksInQue();
+            Console.WriteLine("Enter month: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter month: ");
+            int year = int.Parse(Console.ReadLine());
             Calender calender = new Calender();
+            int[,] array = calender.calender(month, year);
+        }
+        public Queue<List<Object>> CalenderArrayToQue(int m,int y)
+        {
+            string[] week_name = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+                "Friday", "Saturday" };
+
+            Week[] obj = new Week[7];
+            for(int i=0;i<obj.Length;i++)
+            {
+                obj[i] = new Week(week_name[i]);
+            }
+            
+
+            Calender c = new Calender();
+            int d=c.DayOfWeek(1,m,y);
+            int LastDate = c.DaysInMonth(m);
+            int objects = d;
+            for (int i=1;i<=LastDate;i++)
+            {
+                if (i == 1) obj[objects++]= i;
+
+            }
+
+    }
+    }
+    public class Week
+    {
+        public string week;
+        List<int> list = new List<int>();        
+        public Week(string week)
+        {
+            this.week = week;
             
         }
     }
