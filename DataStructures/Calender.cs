47efacf42a354fb30efd5ebd2f4dad036651d069
ashok34589 <ashok34589@gmail.com>
2020-02-27 11:21:20 +0530

    [ashok]refactor. added dayOfWeek() to calender.cs

--- DataStructures/Calender.cs
+++ DataStructures/Calender.cs
@@ -12,6 +12,14 @@ namespace DataStructures
                 return true;
             return false;
         }
+ public int DayOfWeek(int d,int m,int y)
+        {
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+            return d0;
+        }
         public void DisplayArray(int[,] a)
         {
             for(int i=0;i<2;i++)
