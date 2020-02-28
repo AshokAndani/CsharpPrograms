7f0cd4ec4d4e9a330b5f0648c408f364dedbcc26
ashok34589 <ashok34589@gmail.com>
2020-02-28 09:29:32 +0530

    [ashok]refactor. added DriverMethod to QueToStack.cs

--- DataStructures/QueueToStack.cs
+++ DataStructures/QueueToStack.cs
@@ -6,5 +6,18 @@ namespace DataStructures
 {
     class QueueToStack
     {
+	public static void DriverMethod()
+		{
+			QueueToStack q = new QueueToStack();
+			Console.Write("Enter month: ");
+			int m = int.Parse(Console.ReadLine());
+			Console.Write("Enter year: ");
+			int y = int.Parse(Console.ReadLine());
+			Stack<Object> st = q.QueToStack(m,y);
+			q.DisplayQueCalender(st);
+
+			
+			
+		}
     }
 }
