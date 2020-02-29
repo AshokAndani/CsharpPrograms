01a3dc7cb70f03193bb208983dc1a8cbf59ee887
ashok34589 <ashok34589@gmail.com>
2020-02-29 10:05:55 +0530

    [ashok]add. created an implementation class

---
+++ ObjectOrientedProgramming/AddressBook/AddressBookImplementation.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace AddressBook
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using System.IO;
+    public class AddressBookImplementation
+    {
+       
+    /// <summary>
+    ///this is the Person Class which will be serialized 
+    /// </summary>
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int zip { get; set; }
+        public long PhoneNumber { get; set; }
+
+       
+    }
+  
+}
