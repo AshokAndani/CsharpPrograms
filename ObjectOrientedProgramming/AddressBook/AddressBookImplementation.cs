2be1610e5f30340ecfec7160b84cdfd7252b7325
ashok34589 <ashok34589@gmail.com>
2020-03-02 19:19:36 +0530

    [ashok]refactor. created the class in namespace

--- ObjectOrientedProgramming/AddressBook/AddressBookImplementation.cs
+++ ObjectOrientedProgramming/AddressBook/AddressBookImplementation.cs
@@ -1,34 +1,19 @@
-﻿
+﻿/// --------------------------------------------------------------------------------------------------------------------
+/// <copyright file=AddressBookImplementation.cs" company="Bridgelabz">
+///   Copyright © 2020 Company="BridgeLabz"
+/// </copyright>
+/// <creator name="ASHOKKUMAR"/>
+//// --------------------------------------------------------------------------------------------------------------------
 
 namespace AddressBook
 {
-    using System;
-    using System.Collections.Generic;
-    using Newtonsoft.Json;
-    using System.IO;
+    /// <summary>
+    /// this is the implementation of addressBook where Methods are written here 
+    /// </summary>
     public class AddressBookImplementation
     {
-       
     /// <summary>
-    ///this is the Person Class which will be serialized 
+    /// this () adds a new Person to the json file
     /// </summary>
-    class Person
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int zip { get; set; }
-        public long PhoneNumber { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("FirtsName:   {0}\nLastName:    {1}\n" +
-                "PhoneNumber: {2}\nCity:        {3}\nState:       {4}\nZip:         {5}\nAddress:     {6}"
-                , FirstName, LastName,PhoneNumber, City,State,zip,Address);
-        }
-
-    }
-  
+}
 }
