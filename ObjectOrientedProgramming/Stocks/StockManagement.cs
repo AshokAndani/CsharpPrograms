33ee400cb86d5012e7d70ef522823d4015220a5f
ashok34589 <ashok34589@gmail.com>
2020-03-04 12:41:08 +0530

    [ashok ]add. added a new file and added using statements

---
+++ ObjectOrientedProgramming/Stocks/StockManagement.cs
@@ -0,0 +1,12 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=StockManagement.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Stocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
