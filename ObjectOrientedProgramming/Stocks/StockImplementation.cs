10505716ed8f49297fafed94468c04fd0d9f2e66
ashok34589 <ashok34589@gmail.com>
2020-03-04 12:30:27 +0530

    [ashok]add added a new file and created namespace

---
+++ ObjectOrientedProgramming/Stocks/StockImplementation.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=StockImplementation.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Stocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Newtonsoft.Json;
