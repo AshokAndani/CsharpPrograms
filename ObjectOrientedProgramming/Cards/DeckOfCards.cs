0f558512b571e0c3e9361745467e0a5dab715cbd
ashok34589 <ashok34589@gmail.com>
2020-03-04 12:54:49 +0530

    [ashok]add.  written initialization in constructor

--- ObjectOrientedProgramming/Cards/DeckOfCards.cs
+++ ObjectOrientedProgramming/Cards/DeckOfCards.cs
@@ -24,3 +24,25 @@ namespace ObjectOrientedProgramming.Cards
         /// </summary>
         public DeckOfCards()
         {
+
+//// this is the face of the card with numbres indicated
+            string[] face = 
+            {
+                "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Jack", "Queen", "King"
+            };
+
+            ////the suits in string array
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+
+            ////card array
+            this.deck = new Card[this.numberOfCards];
+            this.currentCard = 0;
+            this.randomNumber = new Random();
+
+            ////filling the card array
+            for (int i = 0; i < this.numberOfCards; i++)
+            {
+                this.deck[i] = new Card(face[i % 13], suits[i % 3]);
+            }
+        }
