69fdeb323ecc81fc9364149b2a2a8358edd00691
ashok34589 <ashok34589@gmail.com>
2020-03-04 11:47:54 +0530

    [ashok]add. added a new Account class

---
+++ ObjectOrientedProgramming/CommercialDataProcessing/Account.cs
@@ -0,0 +1,13 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Account.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CommercialDataProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
