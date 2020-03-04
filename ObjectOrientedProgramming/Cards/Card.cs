ba1f89c5ff21bbc64ccb274f58431b93a68b50c6
ashok34589 <ashok34589@gmail.com>
2020-03-04 12:49:06 +0530

    [ashok]add. declared some variables

--- ObjectOrientedProgramming/Cards/Card.cs
+++ ObjectOrientedProgramming/Cards/Card.cs
@@ -15,3 +15,13 @@ namespace ObjectOrientedProgramming.Cards
     public class Card
     {
                 private string suit, value;
+/// <summary>
+        /// card Constructor
+        /// </summary>
+        /// <param name="tvalue">value to input</param>
+        /// <param name="tsuit">suit to input</param>
+        public Card(string tvalue, string tsuit)
+        {
+            this.suit = tsuit;
+            this.value = tvalue;
+        }
