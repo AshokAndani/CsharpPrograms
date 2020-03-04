28f51fb9488fae99ace791dc601ba8b64c79ab99
ashok34589 <ashok34589@gmail.com>
2020-03-04 12:22:05 +0530

    [ashok]add added a new Stock.cs file

---
+++ ObjectOrientedProgramming/Stocks/Stock.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Stock.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Stocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// this class represents information of an Stock
+    /// </summary>
+    public class Stock
+    {
+        public String name { get; set; }
+        public int NumberOfShares { get; set; }
+        public int SharePrice { get; set; }
+        public int StockPrice { get; set; }
+
+        /// <summary>
+        /// string representation of Stock class
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\nPrice: {1}\nNumber of Shares: {2}", name, SharePrice, NumberOfShares);
+        }
+    }
+
+    /// <summary>
+    /// this class contains the list of stocks and total stock price
+    /// </summary>
+    public class StockPortfolio
+    {
+        public int grandTotal { get; set; }
+       public List<Stock> StockList;
+    }
+}
