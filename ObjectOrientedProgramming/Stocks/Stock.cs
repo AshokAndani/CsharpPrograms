11231cc2d59fb821c522262f58b806ad6fd3a59a
ashok34589 <ashok34589@gmail.com>
2020-03-04 12:26:14 +0530

    [ashok]add. added a class to the namespace

--- ObjectOrientedProgramming/Stocks/Stock.cs
+++ ObjectOrientedProgramming/Stocks/Stock.cs
@@ -9,33 +9,8 @@ namespace Stocks
     using System;
     using System.Collections.Generic;
     using System.Text;
-
-    /// <summary>
+ /// <summary>
     /// this class represents information of an Stock
     /// </summary>
     public class Stock
     {
-        public String name { get; set; }
-        public int NumberOfShares { get; set; }
-        public int SharePrice { get; set; }
-        public int StockPrice { get; set; }
-
-        /// <summary>
-        /// string representation of Stock class
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return String.Format("Name: {0}\nPrice: {1}\nNumber of Shares: {2}", name, SharePrice, NumberOfShares);
-        }
-    }
-
-    /// <summary>
-    /// this class contains the list of stocks and total stock price
-    /// </summary>
-    public class StockPortfolio
-    {
-        public int grandTotal { get; set; }
-       public List<Stock> StockList;
-    }
-}
