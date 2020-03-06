568005b7348f9923da21bf7a5d070d0440d4150c
AshokAndani <ashok34589@gmail.com>
2020-03-06 19:08:58 +0530

    [ashok]add Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using DesignPatterns.Creational.SingletonPattern;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LazyInitialization l = LazyInitialization.Instance;
+
+        }
+    }
+}
