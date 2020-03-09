764bd888fc82a2ae9017e911b87ce435438589f2
AshokAndani <ashok34589@gmail.com>
2020-03-09 19:24:55 +0530

    [ashok]add. added Computer.cs file

---
+++ DesignPatterns/Creational/FactoryPattern/Computer.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Computer.cs" company="BridgeLabz">
+//     BridgeLabz. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.FactoryPattern
+{
+    using System;
+
+    /// <summary>
+    ///  this is the abstract class which has some abstract methods
+    /// </summary>
+    public abstract class Computer
+    {
+        public abstract string GetRAM();
+
+        public abstract string GetHDD();
+
+        public abstract string GetCPU();
+
+        /// <summary>
+        /// overridden ToString () to represent the Object state to string format
+        /// </summary>
+        /// <returns>string</returns>
+        public override string ToString()
+        {
+            return "RAM capacity is = " + this.GetRAM() + "   CPU =   " + this.GetCPU() + "  HDD capacity is = " + this.GetHDD();
+        }
+    }
+}
