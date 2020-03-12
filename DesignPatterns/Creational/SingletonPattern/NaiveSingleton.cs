04cab2168cd77c0602534bc6cdb128bfb9ae4eb9
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:23:35 +0530

    [ashok]add. created the NaiveInitialization.cs file

---
+++ DesignPatterns/Creational/SingletonPattern/NaiveSingleton.cs
@@ -0,0 +1,7 @@
+﻿/// <copyright file="NaiveSingleton.cs" company="BridgeLabz">
+///     BridgeLabz. All rights reserved.
+/// </copyright>
+/// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.SingletonPattern
+{
+    using System;
