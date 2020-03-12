6a7dd378bef0c09212a800d709ffdb868263cd58
AshokAndani <ashok34589@gmail.com>
2020-03-12 11:11:28 +0530

    [ashok]add. created XMLMaker.cs file

---
+++ DesignPatterns/Structural/Adapter/XMLMaker.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file="XMLMaker.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Adapter
+{
+    using System;
+    using System.Xml.Linq;
+    using System.Linq;
