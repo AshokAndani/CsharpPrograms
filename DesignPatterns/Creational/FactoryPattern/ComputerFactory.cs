cc3a52bd831d2e975dd0daf33ab20ef7770c5d65
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:12:00 +0530

    [ashok]add created ComputerFactory.cs file

---
+++ DesignPatterns/Creational/FactoryPattern/ComputerFactory.cs
@@ -0,0 +1,6 @@
+﻿// <copyright file="ComputerFactory.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.FactoryPattern
+{
