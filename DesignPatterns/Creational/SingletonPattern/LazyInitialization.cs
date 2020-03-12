d92e252e9b58a39e2c6bc1a2360f18066abf2e53
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:12:58 +0530

    [ashok]add. added LazyInitialization.cs file

---
+++ DesignPatterns/Creational/SingletonPattern/LazyInitialization.cs
@@ -0,0 +1,7 @@
+﻿/// <copyright file="LazyInitialization.cs" company="BridgeLabz">
+///     BridgeLabz. All rights reserved.
+/// </copyright>
+/// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.SingletonPattern
+{
+    using System;
