7423fef77efd98e0688696f92a33b1a2bedab56e
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:16:18 +0530

    [ashok]add. added CarBody.cs

---
+++ DesignPatterns/Structural/Facade/CarBody.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="CarBody.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Facade
+{
+    using System;
+
+    /// <summary>
+    ///  this class is the sub-system class for Car Product
+    /// </summary>
+    public class CarBody
+    {
+
+        /// <summary>
+        ///  this method gets the car Body
+        /// </summary>
+      public  void GetCarBody()
+        {
+            Console.WriteLine("Getting the CarBody");
+        }
+    }
+}
