5a509b1dc6762d4d1aee80279d0ca911b67cba57
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:33:29 +0530

    [ashok]add. added IItemElement.cs

---
+++ DesignPatterns/Behavioral/Visitor/IItemElement.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="ItemElement.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Behavioral.Visitor
+{
+    using System;
+   
+    /// <summary>
+    ///  this is the interface that every concrete classes should implement
+    /// </summary>
+    public interface IItemElement
+    {
+        public int Accept(IShoppingCartVisitor visitor);
+    }
+}
