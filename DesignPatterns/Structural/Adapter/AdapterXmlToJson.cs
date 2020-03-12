d6c2a0a9d67d1abfdd156c670362a0be35073b97
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:55:15 +0530

    [ashok]add. added AdapterXmlToJson class

--- DesignPatterns/Structural/Adapter/AdapterXmlToJson.cs
+++ DesignPatterns/Structural/Adapter/AdapterXmlToJson.cs
@@ -8,3 +8,9 @@ namespace DesignPatterns.Structural.Adapter
     using System.Xml.Linq;
     using System.Linq;
     using Newtonsoft.Json;
+    /// <summary>
+    ///  this class the called as adapter because this bridges the gap between the code which cannot be changed but the client
+    ///  needs the change in output
+    /// </summary>
+    public class AdapterXmlToJson : XMLMaker, IXmlToJson
+    {
