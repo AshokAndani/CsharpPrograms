35715e6884b7570ed1cd1bd05ad711c8519b745b
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:37:21 +0530

    [ashok]add. created PersonDeepCopy.cs file

---
+++ DesignPatterns/Creational/PrototypeDesignPattern/PersonDeepCopy.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="PersonDeepCopy.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.PrototypeDesignPattern
+{
+    using System;
