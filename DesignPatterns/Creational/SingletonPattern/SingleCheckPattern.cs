abc3eee3f36ad6bbf01644946f6c8b497c3ba00c
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:36:28 +0530

    [ashok]add. added SingleLockPattern class

--- DesignPatterns/Creational/SingletonPattern/SingleCheckPattern.cs
+++ DesignPatterns/Creational/SingletonPattern/SingleCheckPattern.cs
@@ -6,4 +6,9 @@ namespace DesignPatterns.Creational.SingletonPattern
 {
     using System;
     using System.Threading;
-    
+        /// <summary>
+    ///  this is class contains the logic to make it totally thread safe but the Performance will be Very slow
+    ///  as everytime the thread has to check for lock and to take out the lock on the Object(which is used as a locker).
+    /// </summary>
+    public sealed class SingleCheckPattern
+    {
