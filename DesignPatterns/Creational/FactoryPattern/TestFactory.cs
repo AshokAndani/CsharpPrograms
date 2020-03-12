62b76e5a3c26c74f17614106c56d74cbdbe7489d
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:25:46 +0530

    [ashok]add. created TestFactory.cs file

---
+++ DesignPatterns/Creational/FactoryPattern/TestFactory.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="TestFactory.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.FactoryPattern
+{
+    using System;
