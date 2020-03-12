134e4b44f87e04130569f276f38de04963c960f6
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:39:08 +0530

    [ashok]add. added Mobile class

--- DesignPatterns/Behavioral/Observer/Mobile.cs
+++ DesignPatterns/Behavioral/Observer/Mobile.cs
@@ -5,3 +5,9 @@
 namespace DesignPatterns.Behavioral.Observer
 {
     using System;
+
+    /// <summary>
+    /// this class extends the Mobileshop which is created when the customer needs the mobile phone
+    /// </summary>
+    public class Mobile : MobileShop
+    {
