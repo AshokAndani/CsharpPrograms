46358369c068e9b2cfa04caf4876c29997ece979
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:52:27 +0530

    [ashok]add. written the logic to test the adapter pattern in AdapterTest constructor

--- DesignPatterns/Structural/Adapter/AdapterTest.cs
+++ DesignPatterns/Structural/Adapter/AdapterTest.cs
@@ -11,3 +11,24 @@ namespace DesignPatterns.Structural.Adapter
     /// </summary>
     public class AdapterTest
     {
+        /// <summary>
+        /// Written the whole in the Constuctor
+        /// </summary>
+        public AdapterTest()
+        {
+            //// XmlMaker is responsible to convert the List of Objects to Xml format
+            XMLMaker make = new XMLMaker();
+            Console.WriteLine("this is the XML file generating code which is legacy which cannot be deleted");
+            //// XmlMaker has a GetXml() which takes the list as and converts into xml format
+            XDocument document = make.GetXml();
+            Console.WriteLine( document+"\n this is json file which is required for the customer_________________" );
+            
+            //// printing the json which is according to client without any change in source code
+            //// here calling the AdapterXmlToJson using its super interface
+            IXmlToJson xmlToJson = new AdapterXmlToJson();
+
+            //// Adapter class contains a Convert method which converts the xml format to json string
+            Console.WriteLine(xmlToJson.GetData(document)) ;
+        }
+    }
+}
