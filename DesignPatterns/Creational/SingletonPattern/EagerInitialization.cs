20151d64243c46ddd84e902cfff21751d3b52bf3
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:07:54 +0530

    [ashok]add. created EagerPattern.cs file

---
+++ DesignPatterns/Creational/SingletonPattern/EagerInitialization.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="EagerInitialization.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.SingletonPattern
+{
+    using System;
