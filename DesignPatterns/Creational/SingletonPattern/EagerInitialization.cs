4df3a3fc8bacb06630f6581e0eb17a3fbe8f5964
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:09:20 +0530

    [ashok].add added a EagerInitialixation class:

--- DesignPatterns/Creational/SingletonPattern/EagerInitialization.cs
+++ DesignPatterns/Creational/SingletonPattern/EagerInitialization.cs
@@ -5,3 +5,7 @@
 namespace DesignPatterns.Creational.SingletonPattern
 {
     using System;
+    public sealed class EagerInitialization
+    {
+        //// this is to check how many times the Constructor is called
+        private static int count = 0;
