e062f4e301e0a6b96661252ee1f047263075df06
AshokAndani <ashok34589@gmail.com>
2020-03-12 11:12:58 +0530

    [ashok]add. added the GetXml()

--- DesignPatterns/Structural/Adapter/XMLMaker.cs
+++ DesignPatterns/Structural/Adapter/XMLMaker.cs
@@ -12,3 +12,22 @@ namespace DesignPatterns.Structural.Adapter
     /// </summary>
     public class XMLMaker
     {
+        /// <summary>
+        ///  this method gives the xml format by taking the list as input
+        /// </summary>
+        /// <returns></returns>
+        public XDocument GetXml()
+        {
+            var list = ManufacturerData.GetManufacturers();
+            XElement xElement = new XElement("Manufacturers");
+            foreach (Manufacturer m in list)
+            {
+                XElement e= new XElement("Manufacturer", new XAttribute("Name", m.name), new XAttribute("Place", m.place));
+                xElement.Add(e);
+            }
+            XDocument xDocument = new XDocument();
+            xDocument.Add(xElement);
+            return xDocument;
+        }
+    }
+}
