bbe5fd090ccc2f8db8469d61124c8758c8ed2d51
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:51:37 +0530

    [ashok]add. created AdapterTest class

--- DesignPatterns/Structural/Adapter/AdapterTest.cs
+++ DesignPatterns/Structural/Adapter/AdapterTest.cs
@@ -6,3 +6,8 @@ namespace DesignPatterns.Structural.Adapter
 {
     using System;
     using System.Xml.Linq;
+    /// <summary>
+    ///  this class simulates the adapter Pattern
+    /// </summary>
+    public class AdapterTest
+    {
