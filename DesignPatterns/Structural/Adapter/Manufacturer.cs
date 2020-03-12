d71d1d1588fb90dd7db6b86ec6c41d131da9f833
AshokAndani <ashok34589@gmail.com>
2020-03-12 11:01:11 +0530

    [ashok]add. added Manufacturer class

---
+++ DesignPatterns/Structural/Adapter/Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Manufacturer.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Adapter
+{
+    using System;
+
+    /// <summary>
+    ///  Manufacturer holds the fields
+    /// </summary>
+    public class Manufacturer
+    {
+        public string name;
+        public string place;
+    }
+}
