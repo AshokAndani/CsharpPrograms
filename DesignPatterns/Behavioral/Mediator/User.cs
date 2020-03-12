71bd58720dfe1a384b0863f079bf77570c2e2239
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:30:37 +0530

    [ashok]add. added User.cs

---
+++ DesignPatterns/Behavioral/Mediator/User.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="User.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Behavioral.Mediator
+{
+    using System;
+
+    /// <summary>
+    /// this abstract class resembles the user Objects and its function declared
+    /// </summary>
+    public abstract class User
+    {
+        protected IChatMediator mediator;
+        protected string name;
+
+        /// <summary>
+        /// constructor to initialize the variables
+        /// </summary>
+        /// <param name="mediator"></param>
+        /// <param name="name"></param>
+        public User(IChatMediator mediator, String name)
+        {
+            this.mediator = mediator;
+            this.name = name;
+        }
+
+        /// <summary>
+        /// a normal function to sending the message
+        /// </summary>
+        /// <param name="message"></param>
+        public abstract void Send(string message);
+
+        /// <summary>
+        /// a normal function to recieving the message
+        /// </summary>
+        /// <param name="message"></param>
+        public abstract void Recieve(string message);
+    }
+}
