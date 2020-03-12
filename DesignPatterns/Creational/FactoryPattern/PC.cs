c53262459956aba928231f882f5d6617ef9ba960
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:17:17 +0530

    [ashok]add. creared PC.cs file

---
+++ DesignPatterns/Creational/FactoryPattern/PC.cs
@@ -0,0 +1,6 @@
+﻿// <copyright file="PC.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.FactoryPattern
+{
