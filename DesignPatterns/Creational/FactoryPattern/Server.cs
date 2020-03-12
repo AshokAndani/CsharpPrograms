a50cb9c3394509586f632d732b9f8a8e1fe26bc7
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:21:25 +0530

    [ashok]add. created a Server.cs File

---
+++ DesignPatterns/Creational/FactoryPattern/Server.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="Server.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.FactoryPattern
+{
+    using System;
