4b46f877f9b147dbb9c37a4c03f66a62645dc24f
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:10:36 +0530

    [ashok].add added a private constructor

--- DesignPatterns/Creational/SingletonPattern/EagerInitialization.cs
+++ DesignPatterns/Creational/SingletonPattern/EagerInitialization.cs
@@ -9,3 +9,12 @@ namespace DesignPatterns.Creational.SingletonPattern
     {
         //// this is to check how many times the Constructor is called
         private static int count = 0;
+	/// <summary>
+        /// private that no one can call this constructor and create the instance of this class
+        /// and this is thread safe only in C# because CLR takes care of Thread accessing the instances
+        /// </summary>
+        private EagerInitialization()
+        {
+            //// to check the Object created or not
+            Console.WriteLine("Eager Pattern is created " + ++count);
+        }
