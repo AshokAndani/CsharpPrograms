93c3ade4161edc384d082ca97fb83d44eafb00a4
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:35:48 +0530

    [ashok]add. created SingleLockPattern.cs file

---
+++ DesignPatterns/Creational/SingletonPattern/SingleCheckPattern.cs
@@ -0,0 +1,9 @@
+﻿/// <copyright file="SingleLockPattern.cs" company="BridgeLabz">
+///     BridgeLabz. All rights reserved.
+/// </copyright>
+/// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.SingletonPattern
+{
+    using System;
+    using System.Threading;
+    
