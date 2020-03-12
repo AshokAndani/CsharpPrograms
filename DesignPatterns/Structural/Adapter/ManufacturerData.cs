0d03787324d41cecb33e1292f2ce1145d521710a
AshokAndani <ashok34589@gmail.com>
2020-03-12 11:03:57 +0530

    [ashok]add. added ManufacturerData.cs File

---
+++ DesignPatterns/Structural/Adapter/ManufacturerData.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="ManufacturerData.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Adapter
+{
+    using System;
+    using System.Collections.Generic;
