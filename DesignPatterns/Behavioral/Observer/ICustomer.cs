88c86684f5d58bedb4ee50292fcd59da87f7f173
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:50:04 +0530

    [ashok]add. added Icustomer interface and declares a UPdate Method

--- DesignPatterns/Behavioral/Observer/ICustomer.cs
+++ DesignPatterns/Behavioral/Observer/ICustomer.cs
@@ -5,3 +5,15 @@
 namespace DesignPatterns.Behavioral.Observer
 {
     using System;
+    /// <summary>
+    ///  this is the interface which has to be implemented
+    /// </summary>
+    public interface ICustomer
+    {
+        /// <summary>
+        /// update method for the customers
+        /// </summary>
+        /// <param name="shop">about the Mobile availablity</param>
+         void Update(MobileShop shop);
+    }
+}
