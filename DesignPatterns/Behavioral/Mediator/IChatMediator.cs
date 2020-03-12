edd66b68adde2cbd2887a6e32aa7adfd30f24d60
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:29:47 +0530

    [ashok]add. added IChatMediator  interface

---
+++ DesignPatterns/Behavioral/Mediator/IChatMediator.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IChatMediator.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Behavioral.Mediator
+{
+    using System;
+
+    /// <summary>
+    /// this is the interface which holds the declared methods which cleint needs
+    /// </summary>
+    public interface IChatMediator
+    {
+        public void SendMessage(String msg, User user);
+        public void AddUser(User user);
+    }
+}
