d8d6d32c30658d4f5f42f54986884918fc5fc99c
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:14:19 +0530

    [ashok]refactor. added computerFactory class

--- DesignPatterns/Creational/FactoryPattern/ComputerFactory.cs
+++ DesignPatterns/Creational/FactoryPattern/ComputerFactory.cs
@@ -4,3 +4,11 @@
 // <author>ASHOKKUMAR</author>
 namespace DesignPatterns.Creational.FactoryPattern
 {
+
+    using System;
+
+    /// <summary>
+    ///  the class which is responsible to decide which class to be invoked based on type input
+    /// </summary>
+    public class ComputerFactory
+    {
