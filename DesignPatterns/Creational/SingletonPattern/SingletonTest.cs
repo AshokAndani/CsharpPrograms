218d7a9b0748b229735b1283fc896e5a4a34e7a5
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:39:52 +0530

    [ashok]add. created the Singleton.cs file

---
+++ DesignPatterns/Creational/SingletonPattern/SingletonTest.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="Singleton.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.SingletonPattern
+{
+    using System;
+    using System.Threading.Tasks;
