a6a0dfc1103777b02132be6f7f22ad415619daa6
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:22:34 +0530

    [ashok]add. added ProxyTest.cs

---
+++ DesignPatterns/Structural/Proxy/ProxyTest.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ProxyTest.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Proxy
+{
+    using System;
+
+    /// <summary>
+    /// this class is used to test the Proxy design Pattern
+    /// </summary>
+    public  class ProxyTest
+    {
+        /// <summary>
+        /// simple static method to run without creating this class object
+        /// </summary>
+        public static void DriverMethod()
+        {
+            ProxyMath proxyMath = new ProxyMath();
+            Console.WriteLine(proxyMath.Add(1, 3)) ;
+        }
+    }
+}
