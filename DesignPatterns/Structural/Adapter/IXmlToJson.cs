4135cdef3614a1f653912aff4b3602bd8394f647
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:57:51 +0530

    [ashok]add. added the IXmlToJson Inteface

---
+++ DesignPatterns/Structural/Adapter/IXmlToJson.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="IXmlToJson.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Adapter
+{
+    using System;
+    using System.Xml.Linq;
+
+    /// <summary>
+    ///  the interface which has the allow the class to convert the xml to json
+    /// </summary>
+    public interface IXmlToJson
+    {
+        /// <summary>
+        ///  method declared which converts the xml to json
+        /// </summary>
+        /// <param name="xDocument"></param>
+        /// <returns>json string</returns>
+        string GetData(XDocument xDocument);
+
+    }
+}
