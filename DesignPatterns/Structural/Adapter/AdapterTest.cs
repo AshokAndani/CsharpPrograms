59e4e59b4b248c30ccd68d7eb3fcc568fae5cdf1
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:51:09 +0530

    [ashok]add. created Adapter.cs file

---
+++ DesignPatterns/Structural/Adapter/AdapterTest.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="AdapterTest.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Adapter
+{
+    using System;
+    using System.Xml.Linq;
