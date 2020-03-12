572a4d3a459ed0e8397351af7eadb68c4116ff14
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:41:03 +0530

    [ashok]add. added PersonshallowCopy class

--- DesignPatterns/Creational/PrototypeDesignPattern/PersonShallowCopy.cs
+++ DesignPatterns/Creational/PrototypeDesignPattern/PersonShallowCopy.cs
@@ -5,3 +5,9 @@
 namespace DesignPatterns.Creational.PrototypeDesignPattern
 {
     using System;
+
+    /// <summary>
+    /// this class simulates the example for shallow cloning where change in clone effects the original copy
+    /// </summary>
+    public class PersonShallowCopy : ICloneable
+    {
