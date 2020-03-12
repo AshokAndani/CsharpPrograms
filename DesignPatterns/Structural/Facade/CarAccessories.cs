f6dba21211934976ac0dc36d067fb7191eeb436e
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:15:58 +0530

    [ashok]add. added CarAccessories.cs

---
+++ DesignPatterns/Structural/Facade/CarAccessories.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="CarAccessories.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Facade
+{
+    using System;
+
+    /// <summary>
+    ///  this class is a sub-system class for the car Product
+    /// </summary>
+    public class CarAccessories
+    {
+        /// <summary>
+        ///  method for getting the Car Accessories
+        /// </summary>
+        public void GetAccessories()
+        {
+            Console.WriteLine("Getting the Accessories");
+        }
+    }
+}
+
