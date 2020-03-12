e0419bb41603f584f806e96c9130f6b61db6500f
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:27:42 +0530

    [ashok]add.completed the logics for TestFactory class

--- DesignPatterns/Creational/FactoryPattern/TestFactory.cs
+++ DesignPatterns/Creational/FactoryPattern/TestFactory.cs
@@ -10,3 +10,24 @@ namespace DesignPatterns.Creational.FactoryPattern
     /// </summary>
     public class TestFactory
     {
+     /// <summary>
+        /// to run the code without creating the object of this class
+        /// </summary>
+        public static void DriverMethod()
+        {
+            //// this variable has the example for generating the PC object as the type is PC
+            Computer pc = ComputerFactory.GetComputer("PC", "Intel iCore", "128GB", "8GB");
+            Console.WriteLine(pc);
+            Console.WriteLine(pc.GetType());
+
+            //// this variable has the example for generating the Server Object as the type is Server
+            Computer server = ComputerFactory.GetComputer("Server", "AMD Processor", "2TB", "8GB");
+            Console.WriteLine(server);
+            Console.WriteLine(server.GetType());
+
+            //// this variable has the example which prints class not found type doesn't exists
+            Computer Foo = ComputerFactory.GetComputer("Foo", "RADEON", "1TB", "4GB");
+            Console.WriteLine(Foo);
+        }
+    }
+}
