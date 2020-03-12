7bf5c99d423e87cca395b8ca525047d72ada26d6
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:19:26 +0530

    [ashok]add. added FacadeTest.cs

---
+++ DesignPatterns/Structural/Facade/FacadeTest.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="FacadeTest.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Facade
+{
+    using System;
+    /// <summary>
+    /// this is the interface which is used by the user to get the things done without thinking about complexity of sub-systems
+    /// </summary>
+    public class FacadeTest
+    {
+        /// <summary>
+        ///  this is a simple static method which is used to run without creating this class
+        /// </summary>
+       public static void DriverMethod()
+        {
+            //// this is the client interface which client will use just by creating the Facade class Object
+            Facade facade = new Facade();
+            Console.WriteLine("the parts required for making a car");
+            //// just by calling the Facade Mathod the car having different subsystem of class with thier methods are invoked
+            ////using the Facade class
+            facade.MakeCar();
+        }
+    }
+}
