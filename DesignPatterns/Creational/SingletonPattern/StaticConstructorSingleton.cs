0ec6ee4cfce44cb19d7eaec45176f4d28f3e572a
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:44:16 +0530

    [ashok]add. created StaticConstructorSingleton.cs file

---
+++ DesignPatterns/Creational/SingletonPattern/StaticConstructorSingleton.cs
@@ -0,0 +1,7 @@
+﻿/// <copyright file="StaticConstructorSingleton.cs" company="BridgeLabz">
+///     BridgeLabz. All rights reserved.
+/// </copyright>
+/// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Creational.SingletonPattern
+{
+    using System;
