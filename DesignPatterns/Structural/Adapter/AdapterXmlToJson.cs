390bc48470ecebd9e9218eb04eaa353dadf7856c
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:54:23 +0530

    [ashok]add. created AdapterXmlToJson.cs file

---
+++ DesignPatterns/Structural/Adapter/AdapterXmlToJson.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="AdapterXmlToJson.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.Structural.Adapter
+{
+    using System;
+    using System.Xml.Linq;
+    using System.Linq;
+    using Newtonsoft.Json;
