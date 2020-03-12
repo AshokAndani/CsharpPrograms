0483b78b796c62b75f991d16bdaa7aa18cc85869
AshokAndani <ashok34589@gmail.com>
2020-03-12 09:15:22 +0530

    [ashok]refactor. added a helper factory method to factory

--- DesignPatterns/Creational/FactoryPattern/ComputerFactory.cs
+++ DesignPatterns/Creational/FactoryPattern/ComputerFactory.cs
@@ -12,3 +12,27 @@ namespace DesignPatterns.Creational.FactoryPattern
     /// </summary>
     public class ComputerFactory
     {
+
+        /// <summary>
+        /// this is the main static factory method that binds the Objects on type
+        /// </summary>
+        /// <param name="type">the classname</param>
+        /// <param name="cpu">cpu type</param>
+        /// <param name="hdd">hdd capacity</param>
+        /// <param name="ram">ram size</param>
+        /// <returns></returns>
+        public static Computer GetComputer(string type, string cpu, string hdd, string ram)
+        {
+            if (type.Equals("PC"))
+            {
+                return new PC(cpu, hdd, ram);
+            }
+            else if (type.Equals("Server"))
+            {
+                return new Server(cpu, hdd, ram);
+            }
+            else Console.WriteLine("Given class not found");
+            return null;
+        }
+    }
+}
