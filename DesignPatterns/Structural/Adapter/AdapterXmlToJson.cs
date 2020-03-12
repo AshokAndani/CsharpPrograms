27e9bf52b4d426ed1cc9812f2842f40129cba1c0
AshokAndani <ashok34589@gmail.com>
2020-03-12 10:56:25 +0530

    [ashok]add. added the new method that changes xml to json without affecting its source code

--- DesignPatterns/Structural/Adapter/AdapterXmlToJson.cs
+++ DesignPatterns/Structural/Adapter/AdapterXmlToJson.cs
@@ -14,3 +14,22 @@ namespace DesignPatterns.Structural.Adapter
     /// </summary>
     public class AdapterXmlToJson : XMLMaker, IXmlToJson
     {
+        /// <summary>
+        /// the Convert Method converts the Xml document to the json string
+        /// </summary>
+        /// <param name="xDocument"></param>
+        /// <returns>json string</returns>
+        public string GetData(XDocument xDocument)
+        {
+            //// var binds the type at runtime
+            //// Select is a method from the System.Linq Library which is specially used for IEnumerables
+            var serializable = xDocument.Element("Manufacturers").Elements("Manufacturer").Select(m => new Manufacturer
+            {
+                //// fetching the value from the Attribute and assigning it to the object again
+                name = m.Attribute("Name").Value,
+                place = m.Attribute("Place").Value
+            });
+            return JsonConvert.SerializeObject(serializable, Formatting.Indented);
+        }
+    }
+}
