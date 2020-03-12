5a85d9ea8d09e72f2165b3bc0b8919fa4d85d8fc
AshokAndani <ashok34589@gmail.com>
2020-03-12 15:48:17 +0530

    [ashok]add. added a static method to run the code

--- DesignPatterns/Behavioral/Observer/ObserverTest.cs
+++ DesignPatterns/Behavioral/Observer/ObserverTest.cs
@@ -5,3 +5,20 @@
 namespace DesignPatterns.Behavioral.Observer
 {
     using System;
+    /// <summary>
+    /// this class is used to run the test Observer Pattern
+    /// </summary>
+    public class ObserverTest
+    {
+        public static void DriverMethod()
+        {
+            Mobile redmi = new Mobile("Samsung");
+            redmi.Attach(new CustomerImpl("ashok"));
+            redmi.Attach(new CustomerImpl("Anil"));
+            redmi.Attach(new CustomerImpl("ramesh"));
+            redmi.Attach(new CustomerImpl("Shivaraj"));
+
+            redmi.Availability = true;
+        }
+    }
+}
