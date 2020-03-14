61b58f5c3851160c453bf308c164e76a6923b250
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:46:44 +0530

    [ashok]add. added the IUser interface and a abstract method, a string property

--- AutofacDemo/DEWithAutofac/Iuser.cs
+++ AutofacDemo/DEWithAutofac/Iuser.cs
@@ -7,3 +7,21 @@ namespace AutofacDemo.DEWithAutofac
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// this interface has a method to change the username
+    /// </summary>
+    interface IUser
+    {
+        /// <summary>
+        /// username property
+        /// </summary>
+        public string UserName { get;  set; }
+
+        /// <summary>
+        /// method to change the username
+        /// </summary>
+        /// <param name="username"></param>
+        public void ChangeUserName(string username);
+    }
+}
