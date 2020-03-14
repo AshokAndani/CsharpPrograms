791fe3d18bf660f0b09ce90499fb87f97e6fd6c5
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:38:38 +0530

    [ashok]add added the NotificationServiceImpl class

---
+++ AutofacDemo/DependencyInjection/NotificationServiceImpl.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="NotificationServiceImpl.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace AutofacDemo.NotificationApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// this class is to provide notification of username change
+    /// </summary>
+    public class NotificationServiceImpl : INotificationService
+    {
+        /// <summary>
+        /// the methods which is used to find the username change
+        /// </summary>
+        /// <param name="user"></param>
+        public void UserNameChanged(IUser user)
+        {
+            //// prints to console whenever the username is changed
+            Console.WriteLine("Notification: username changed to : "+user.Username);
+        }
+    }
+}
