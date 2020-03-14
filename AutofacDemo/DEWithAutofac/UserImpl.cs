885ba69c1e592c2e615cdd5b904f1b532f066f24
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:50:47 +0530

    [ashok]add. added UserImpl class

--- AutofacDemo/DEWithAutofac/UserImpl.cs
+++ AutofacDemo/DEWithAutofac/UserImpl.cs
@@ -7,3 +7,9 @@ namespace AutofacDemo.DEWithAutofac
     using System;
     using System.Collections.Generic;
     using System.Text;
+    
+    /// <summary>
+    /// this class is the one where username resides to operate on it
+    /// </summary>
+    class UserImpl : IUser
+    {
