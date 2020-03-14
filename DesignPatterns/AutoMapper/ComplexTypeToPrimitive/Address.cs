99c071b6f9ec2fe6eeed40d49e03d444d0680df0
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:05:48 +0530

    [ashok]add. added the Address class

---
+++ DesignPatterns/AutoMapper/ComplexTypeToPrimitive/Address.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="Address.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.AutoMapper.ComplexTypeToPrimitive
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// this class holds the information of the Person
+    /// </summary>
+    public class Address
+    {
+        public string city, state, country;
+
+
+        /// <summary>
+        /// constructor to initialize the fields
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="state"></param>
+        /// <param name="country"></param>
+        public Address(string city, string state, string country)
+        {
+            this.city = city;
+            this.state = state;
+            this.country = country;
+        }
+        public override string ToString()
+        {
+            return string.Format("(Inner class Name){0}------\nCity: {1}\tState: {2}\tCountry: {3}\n", this.GetType().Name, this.city, this.state, this.country);
+        }
+    }
+}
