00492a3a40755cfcce3a75b8eaf02f8fa6209eef
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:50:10 +0530

    [ashok]add. added UserImpl.cs file

---
+++ AutofacDemo/DEWithAutofac/UserImpl.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file="UserImpl.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace AutofacDemo.DEWithAutofac
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
