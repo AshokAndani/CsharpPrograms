2f2641866b5510a30fa270e2d380d963aea5207d
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:45:47 +0530

    [ashok]add. added the IUser.cs file

---
+++ AutofacDemo/DEWithAutofac/Iuser.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file="IUser.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace AutofacDemo.DEWithAutofac
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
