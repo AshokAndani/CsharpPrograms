e4f10c9c1080703284070d620754c47f4ebbbb72
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:36:52 +0530

    [ashok]add added the InotificationService which is an interface

---
+++ AutofacDemo/DependencyInjection/INotificationService.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="INotificationService.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace AutofacDemo.NotificationApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// inteeface for Notification
+    /// </summary>
+    interface INotificationService
+    {
+        /// <summary>
+        /// to notify username change
+        /// </summary>
+        /// <param name="user"></param>
+        public void UserNameChanged(IUser user);
+    }
+}
