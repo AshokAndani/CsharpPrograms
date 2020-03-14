a6630a97044c75605a59137795f1571c4f52d759
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:48:55 +0530

    [ashok]add. added NotificationServiceImpl class, and implemented the abstract method from from its parent interface

--- AutofacDemo/DEWithAutofac/NotificationServiceImpl.cs
+++ AutofacDemo/DEWithAutofac/NotificationServiceImpl.cs
@@ -7,3 +7,19 @@ namespace AutofacDemo.DEWithAutofac
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// this class writes to console about the username change
+    /// </summary>
+    class NotificationServiceImpl : INotificationService
+    {
+        /// <summary>
+        /// this method consists the logic to write to console
+        /// </summary>
+        /// <param name="user"></param>
+        public void UserNameChanged(IUser user)
+        {
+            Console.WriteLine("Notification:   User name changed to "+user.UserName);
+        }
+    }
+}
