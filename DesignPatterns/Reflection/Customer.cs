eb0c4aea458d8247cb225321c29e8f9e388c8c03
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:18:06 +0530

    [ashok]add. Customer class that has some method and fields

---
+++ DesignPatterns/Reflection/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Reflection
+{
+        public class Customer
+    {
+        public string FirstName;
+        public string LastName;
+        public int age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+            }
+        }
+        public bool under18;
+        
+        public string GetFullName(string firstname, string lastName)
+        {
+            return firstname + " " + lastName;
+        }
+        public Customer() { }
+        public Customer(String name, String city)
+        { }
+    }
+}
