919c9cdf8ba06132a6aab9e9a200954b866de205
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:44:39 +0530

    [ashok]add. added the INotification interface with a abstract method

--- AutofacDemo/DEWithAutofac/INotificationService.cs
+++ AutofacDemo/DEWithAutofac/INotificationService.cs
@@ -7,3 +7,16 @@ namespace AutofacDemo.DEWithAutofac
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// this interface declared a usernamechange notification method
+    /// </summary>
+    interface INotificationService
+    {
+        /// <summary>
+        /// to notify the username change
+        /// </summary>
+        /// <param name="user"></param>
+        void UserNameChanged(IUser user);
+    }
+}
