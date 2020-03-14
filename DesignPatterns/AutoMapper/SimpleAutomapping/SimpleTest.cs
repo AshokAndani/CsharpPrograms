653259a10d3442179e4d6e516bff2a5d2e31e38c
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:00:43 +0530

    [ashok]add. added SimpleTest which is user to test the simple mapping

---
+++ DesignPatterns/AutoMapper/SimpleAutomapping/SimpleTest.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="SimpleTest.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.AutoMapper.SimpleAutomapping
+{
+    using global::AutoMapper;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// this class is used to test the mapping
+    /// </summary>
+    public class SimpleTest
+    {
+        /// <summary>
+        /// simple method to run without creating the object of this class
+        /// </summary>
+        public static void DriverMethod()
+        {
+            ////configuring the Mapper
+            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<Customer, Employee>());
+
+            //// creating the mapper Object
+            IMapper mapper = configuration.CreateMapper();
+
+            //// creating the Customer and filling the fields
+            var customer = new Customer("Ashok", "Andani", 23, "Banglore");
+
+            //// Assigning the mapped object
+            var employee = mapper.Map<Employee>(customer);
+
+            //// Printing the final mapped Object and normal object
+            Console.WriteLine(customer.GetType().Name + "-------->\n" + customer + "\n----------------------------------------------\n");
+            Console.WriteLine(employee.GetType().Name + "------->\n" + employee);
+        }
+    }
+}
