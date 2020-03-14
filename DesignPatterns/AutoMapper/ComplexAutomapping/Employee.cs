939d8e86eb2f3dc08dac6ed11aab27a7aab31d13
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:10:23 +0530

    [ashok]add. added Employee sample class

---
+++ DesignPatterns/AutoMapper/ComplexAutomapping/Employee.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Employee.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace DesignPatterns.AutoMapper.ComplexAutomapping
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// this class is mapped with the Customer class
+    /// </summary>
+    public class Employee
+    {
+        public string name;
+        public AddressDTO address;
+
+        public override string ToString()
+        {
+            return string.Format("{0}\nName: {1}\nAddress: {2}\n", this.GetType().Name, this.name, this.address);
+        }
+    }
+}
