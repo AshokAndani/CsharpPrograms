f0f2c537fc154cff982305406bfe70495aa9e34b
AshokAndani <ashok34589@gmail.com>
2020-03-14 15:37:14 +0530

    [ashok]add added the IUser which is an interface

---
+++ AutofacDemo/DependencyInjection/IUser.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="IUser.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+
+
+namespace AutofacDemo.NotificationApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    ///  methods for user
+    /// </summary>
+    public interface IUser
+    {
+        public string Username { get; set; }
+
+        /// <summary>
+        /// this method is to change the username field
+        /// </summary>
+        /// <param name="username">string username</param>
+        public void ChangeUserName(string username);
+    }
+}
