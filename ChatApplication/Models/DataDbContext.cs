ff27b1bdf09d62855abdc315982f6ff708cd0c25
ashok34589 <ashok34589@gmail.com>
2020-04-03 12:29:31 +0530

    [ashok]add. added the DataDbContext class

---
+++ ChatApplication/Models/DataDbContext.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="DataDbContext.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace ChatApplication.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// this classs inherits the DbContext class which controls the database
+    /// </summary>
+    public class DataDbContext : DbContext
+    {
+        /// <summary>
+        /// this is the constructor calls the base class constructor
+        /// </summary>
+        /// <param name="options"></param>
+        public DataDbContext(DbContextOptions<DataDbContext> options): base(options)
+        {
+        }
+
+        /// <summary>
+        /// adding the User class object to DbSet which internally creates the table of user
+        /// </summary>
+        public DbSet<User> users { get; set; }
+    }
+}
