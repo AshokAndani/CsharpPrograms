d65092cbe99d42226a54ddb71f5a00d01486b1e8
ashok34589 <ashok34589@gmail.com>
2020-04-03 12:28:34 +0530

    [ashok]add. added the User class which represents the username and messages

---
+++ ChatApplication/Models/User.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="User.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace ChatApplication.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// this class takes the message and the username and saves that to Database
+    /// </summary>
+    public class User
+    {
+        [Key]
+        public int Id{ get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
