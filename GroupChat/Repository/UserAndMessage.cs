7bf48fcdc3cdc504de85ddbab2a18c0af68e6d91
ashok34589 <ashok34589@gmail.com>
2020-04-04 08:26:23 +0530

    [ashok]add. a simple class represents database table that stores the messages and the usernames

---
+++ GroupChat/Repository/UserAndMessage.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="IMessageHandler.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupChat.Repository
+{
+    public class UserAndMessage
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string  GroupName { get; set; }
+        [Required]
+        public string SocketID { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
