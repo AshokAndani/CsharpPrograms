9b5743409524614296271f30c003e146e7845d44
ashok34589 <ashok34589@gmail.com>
2020-04-04 08:28:02 +0530

    [ashok]add. this class has the implementation for the IMessageHandler interface

---
+++ GroupChat/Repository/MessageHandler.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="IMessageHandler.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace GroupChat.Repository
+{
+    public  class MessageHandler : IMessageHandler
+    {
+        private readonly DatabaseContext context;
+
+        public MessageHandler(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        public void AddMessages(UserAndMessage user)
+        {
+            context.MessagesTb.Add(user);
+            context.SaveChanges();
+        }
+        public void DeleteMessages(string userID)
+        {
+            context.SaveChanges();
+        }
+
+    }
+}
