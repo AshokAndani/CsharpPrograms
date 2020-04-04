8580f4c216589486ba5f3773de962b9486cc873f
ashok34589 <ashok34589@gmail.com>
2020-04-04 08:25:17 +0530

    [ashok]add. this file contains the inherited class for DbContext which contains methods to interact with the database

---
+++ GroupChat/Repository/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="DatabaseContext.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace GroupChat.Repository
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// this class contains the database operations
+    /// </summary>
+    public class DatabaseContext : DbContext
+    {
+        /// <summary>
+        /// constructor calling the base class constructor which takes parameter
+        /// </summary>
+        /// <param name="options"></param>
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// creating the MessagesTb which resembles the database table
+        /// </summary>
+        public DbSet<UserAndMessage> MessagesTb { get; set; }
+    }
+}
