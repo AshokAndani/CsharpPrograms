41d41dabbc882e4cc56aa66b099cceee13d3a369
ashok34589 <ashok34589@gmail.com>
2020-04-06 18:44:44 +0530

    [ashok]add. added the Program.cs entrypoint file of the application

---
+++ JwtAuthorization/JwtAuthorization/Program.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Program.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace JwtAuthorization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// this class is the entry point for the Web Api
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// this method get called by the runtime automatically
+        /// </summary>
+        /// <param name="args">passed arguments</param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// this creates the WebHost which is intern the Web Application
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
