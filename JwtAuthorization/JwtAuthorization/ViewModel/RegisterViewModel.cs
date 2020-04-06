3125df5dc03e5f629a84484486ec0620bc615318
ashok34589 <ashok34589@gmail.com>
2020-04-06 19:00:42 +0530

    [ashok]add. added the RegisterViewModel class

---
+++ JwtAuthorization/JwtAuthorization/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="RegisterViewModel.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace JwtAuthorization.ViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// this class acts as the model while registering the user
+    /// </summary>
+    public class RegisterViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
