65f9e2c85f489636cd2658c029636bf60e5db1e7
ashok34589 <ashok34589@gmail.com>
2020-04-06 18:55:35 +0530

    [ashok]add. added the Context file

---
+++ JwtAuthorization/JwtAuthorization/Repository/ApplcationDbContext.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="ApplicationDbContext.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace JwtAuthorization.Repository
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    
+    /// <summary>
+    /// this class is the instance of the Entity Framework
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        /// <summary>
+        /// the constructor calls the base class constructor with passing the parameters
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// checking wether the data exists in the database or not if not then saves the data
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" }
+                );
+        }
+    }
+}
