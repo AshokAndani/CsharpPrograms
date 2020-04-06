9bc0159dbee01bd874b05ddc739d0b6040e38aea
ashok34589 <ashok34589@gmail.com>
2020-04-06 19:01:02 +0530

    [ashok]add. added the LoginViewModel class

---
+++ JwtAuthorization/JwtAuthorization/ViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="LoginViewModel.cs" company="BridgeLabz">
+//     BridgeLabs. All rights reserved.
+// </copyright>
+// <author>ASHOKKUMAR</author>
+namespace JwtAuthorization.ViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// this class acts as the model during the login of the user
+    /// </summary>
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
