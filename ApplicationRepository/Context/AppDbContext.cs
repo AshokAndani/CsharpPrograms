5b6b3577127f14ecd11dba1e71754a86cda352b1
ashok34589 <ashok34589@gmail.com>
2020-04-11 09:57:35 +0530

    [ashok]add. added the inherited DbContext Class

---
+++ ApplicationRepository/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppDbContext.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ApplicationRepository.Context
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    
+    /// <summary>
+    /// Extended class of IdentitDbContext
+    /// </summary>
+    public class AppDbContext : IdentityDbContext
+    {
+        /// <summary>
+        /// Calling the Super Class Constructor by passing the Parameter
+        /// </summary>
+        /// <param name="options"></param>
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+    }
+}
