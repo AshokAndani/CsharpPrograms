c3515690a610c89941821e5fd018807cb924f30a
ashok34589 <ashok34589@gmail.com>
2020-04-11 10:01:52 +0530

    [ashok]add. added RegisterViewModel class file

---
+++ Common/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RegisterViewModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// Model class holds the data for Registering the New User
+    /// </summary>
+    public class RegisterViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
