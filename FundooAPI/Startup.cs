7c02711a83af224c0b352945f74e40abdc68e643
ashok34589 <ashok34589@gmail.com>
2020-04-12 16:02:49 +0530

    [ashok]refactor. added the swagger and jwt middlewares and services

--- FundooAPI/Startup.cs
+++ FundooAPI/Startup.cs
@@ -9,10 +9,14 @@ namespace FundooAPI
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
     using ApplicationBusiness.Interfaces;
     using ApplicationBusiness.Services;
     using ApplicationRepository.Context;
+    using ApplicationRepository.Interfaces;
+    using ApplicationRepository.Services;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Identity;
@@ -22,7 +26,8 @@ namespace FundooAPI
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
-
+    using Microsoft.IdentityModel.Tokens;
+    using Swashbuckle.AspNetCore.Swagger;
     /// <summary>
     /// this class holds the middlewares and the registered Services
     /// </summary>
@@ -35,22 +40,51 @@ namespace FundooAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            //Database connection
+            ////Database connection
             services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("Connection"),
             p => p.MigrationsAssembly("FundooAPI")));
 
-            //Security Service
+            ////Default Security Service
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>();
-
-            //Registering the classes here
+           ////Registering the classes here
             services.AddTransient<IApplicationUserManager, ApplicationUserManagerImpl>();
+
+            //// for Labels
+            services.AddTransient<ILabelsRepository, LabelsRepositoryImpl>();
+            services.AddTransient<ILabelManager, LabelManagerImpl>();
+
+            ///Jwt security Service
+            var secretKey = Configuration["Jwt:_key"];
+            var symmerticKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:_url"],
+                    ValidAudience=Configuration["Jwt:_url"],
+                    IssuerSigningKey=symmerticKey,
+                    RequireExpirationTime=true
+                    };
+            });
+
+            ////Adding the Swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("V1",new Info {Title="FundooApplication", Version="V1" });
+            });
 
         }
 
@@ -65,6 +99,12 @@ namespace FundooAPI
             app.UseAuthentication();
 
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/V1/Swagger.json","FundooApplication");
+            });
         }
     }
 }
