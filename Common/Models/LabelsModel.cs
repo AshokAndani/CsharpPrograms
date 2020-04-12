924f9674c0caa9be2a7d1f2c5e3850065d145404
ashok34589 <ashok34589@gmail.com>
2020-04-12 16:15:05 +0530

    [ashok]add. added the LabelsModel.cs file

---
+++ Common/Models/LabelsModel.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LabelsModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// this class represents the model for labels operation
+    /// </summary>
+    public class LabelsModel
+    {
+        [Key,]
+        public int Id { get; set; }
+        [Required]
+        public int LBNumber { get; set; }
+        [Required]
+        public string Label { get; set; }
+    }
+}
