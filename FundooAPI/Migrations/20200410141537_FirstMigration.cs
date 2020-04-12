19a9fc0e775d462243e51146f931efd9879c0b82
ashok34589 <ashok34589@gmail.com>
2020-04-12 16:01:22 +0530

    [ashok]refactor. updated the migration

--- FundooAPI/Migrations/20200410141537_FirstMigration.cs
+++ FundooAPI/Migrations/20200410141537_FirstMigration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FundooAPI.Migrations
 {
--- FundooAPI/Migrations/20200412030857_Third.Designer.cs
+++ FundooAPI/Migrations/20200412030857_Third.Designer.cs
@@ -4,14 +4,16 @@ using ApplicationRepository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FundooAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200412030857_Third")]
+    partial class Third
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -24,8 +26,6 @@ namespace FundooAPI.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<int>("LBNumber");
 
                     b.Property<string>("Label")
                         .IsRequired();
