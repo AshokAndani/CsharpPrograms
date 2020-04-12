df24e7fe7af9113beef30fa498cd12d7ce56494b
ashok34589 <ashok34589@gmail.com>
2020-04-12 16:04:52 +0530

    [ashok]add. added the ILabelManager interface

---
+++ ApplicationBusiness/Interfaces/ILabelManager.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ILabelManager.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ApplicationBusiness.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Common.Models;
+
+    /// <summary>
+    /// To Manage the Labels
+    /// </summary>
+    public interface ILabelManager
+    {
+        Task<int> AddLabel(LabelsModel model);
+        Task<int> DeleteLabel(int id);
+        Task<int> UpdateLabel(LabelsModel model);
+        IEnumerable<LabelsModel> GetAllLabels();
+        LabelsModel GetLabel(int id);
+    }
+}
