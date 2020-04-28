37c3256b7d0b0e1479f623995885e37a494e9c25
ashok34589 <ashok34589@gmail.com>
2020-04-28 13:31:33 +0530

    [ashok]refactor updated IApplicationUserManager

--- ApplicationBusiness/Interfaces/IApplicationUserManager.cs
+++ ApplicationBusiness/Interfaces/IApplicationUserManager.cs
@@ -21,15 +21,16 @@ namespace ApplicationBusiness.Interfaces
         /// <summary>
         /// to create the new Users
         /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
+        /// <param name="model">registerModel from body</param>
+        /// <returns>Result
+        /// </returns>
         Task<IdentityResult> CreateUserAsync(RegisterViewModel model);
 
         /// <summary>
         /// loggig in the User
         /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
+        /// <param name="model">LoginModel from body</param>
+        /// <returns>Result</returns>
         Task<SignInResult> LoginUserAsync(LoginViewModel model);
 
         /// <summary>
@@ -41,22 +42,42 @@ namespace ApplicationBusiness.Interfaces
         /// <summary>
         /// Generates the token against the user who forgot the password
         /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task<string> GetResetToken(ForgotPasswordModel model);
+        /// <param name="model">Email</param>
+        /// <returns>Reset Password Token</returns>
+        Task<string> GetResetToken(string email);
 
         /// <summary>
         /// Resets the Password based on the token
         /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
+        /// <param name="model">Info of User</param>
+        /// <returns>Result</returns>
         Task<IdentityResult> ResetPasswordAsync(ResetPasswordModel model);
         
         /// <summary>
         /// Changes the password when a user wants to change the Password
         /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
+        /// <param name="model">ChangePasswordModel</param>
+        /// <returns>Result</returns>
         Task<IdentityResult> ChangePasswordAsync(ChangePasswordModel model);
+
+        /// <summary>
+        /// To Send the Reset Password Token to the MSMQ
+        /// </summary>
+        /// <param name="message">Token</param>
+        void SendMassageToMSMQ(string message);
+
+        /// <summary>
+        /// To Retrieve the Message from MSMQ
+        /// </summary>
+        /// <returns>Token</returns>
+        string RetrieveMessageFromMSMQ();
+
+        /// <summary>
+        /// To send the Reset link to the User
+        /// </summary>
+        /// <param name="link">the link</param>
+        /// <param name="email">email of the user</param>
+        /// <returns>Result</returns>
+        dynamic SendMail(string link,string email);
     }
 }
