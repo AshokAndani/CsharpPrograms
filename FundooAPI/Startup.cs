8649abc369a743c9d8d99bffc94ae05ba6d48aaa
ashok34589 <ashok34589@gmail.com>
2020-04-28 13:23:30 +0530

    [ashok]refactor changed some code in Startup.cs

--- FundooAPI/Startup.cs
+++ FundooAPI/Startup.cs
@@ -16,9 +16,11 @@ namespace FundooAPI
     using ApplicationRepository.Context;
     using ApplicationRepository.Interfaces;
     using ApplicationRepository.Services;
+    using CloudinaryDotNet.Actions;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
@@ -27,6 +29,7 @@ namespace FundooAPI
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
     using Microsoft.IdentityModel.Tokens;
+    using Swashbuckle.AspNetCore.Filters;
     using Swashbuckle.AspNetCore.Swagger;
     /// <summary>
     /// this class holds the middlewares and the registered Services
@@ -51,13 +54,27 @@ namespace FundooAPI
 
             ////Default Security Service
             services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>();
-           ////Registering the classes here
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            //// setting timespan for tokens
+            services.Configure<DataProtectionTokenProviderOptions>(opt =>
+            opt.TokenLifespan = TimeSpan.FromHours(2));
+
+            ////Registering the classes here
             services.AddTransient<IApplicationUserManager, ApplicationUserManagerImpl>();
+
+            //// Identity Classes
+            services.AddTransient<UserManager<IdentityUser>>();
+            services.AddTransient<SignInManager<IdentityUser>>();
 
             //// for Labels
             services.AddTransient<ILabelsRepository, LabelsRepositoryImpl>();
             services.AddTransient<ILabelManager, LabelManagerImpl>();
+
+            /// for Notes
+            services.AddTransient<INotesRepository, NotesRepositoryImpl>();
+            services.AddTransient<INotesManager, NotesManagerImpl>();
 
             ///Jwt security Service
             var secretKey = Configuration["Jwt:_key"];
@@ -69,23 +86,40 @@ namespace FundooAPI
             }).AddJwtBearer(options =>
             {
                 options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
                     ValidateAudience = true,
                     ValidateIssuer = true,
                     ValidateIssuerSigningKey = true,
                     ValidIssuer = Configuration["Jwt:_url"],
-                    ValidAudience=Configuration["Jwt:_url"],
-                    IssuerSigningKey=symmerticKey,
-                    RequireExpirationTime=true
-                    };
+                    ValidAudience = Configuration["Jwt:_url"],
+                    IssuerSigningKey = symmerticKey,
+                    RequireExpirationTime = true
+                };
             });
 
             ////Adding the Swagger
             services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("V1",new Info {Title="FundooApplication", Version="V1" });
+                options.SwaggerDoc("V2", new Swashbuckle.AspNetCore.Swagger.Info { Title = "FundooApplication", Version = "V2" });
+                
+                //// this service handles the jwt token suppying in headers
+                options.AddSecurityDefinition("oauth2", new ApiKeyScheme
+                {
+                    Description = "Using the jwt bearer token",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
             });
 
+            //// Adding Redis cache 
+            services.AddDistributedRedisCache(options=>
+            {
+                options.Configuration = "localhost:6379";
+                options.InstanceName = "FundooNotes";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,17 +127,21 @@ namespace FundooAPI
         {
             if (env.IsDevelopment())
             {
+
+
                 app.UseDeveloperExceptionPage();
             }
 
+            //// Using the Authentication
             app.UseAuthentication();
 
             app.UseMvc();
-
+            //// Using the Swagger
             app.UseSwagger();
+            //// Setting up the Swagger Endpoints
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/V1/Swagger.json","FundooApplication");
+                options.SwaggerEndpoint("/swagger/V2/Swagger.json", "FundooApplication");
             });
         }
     }
