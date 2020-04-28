872895543de30dc9c7b0ad9c7e1f546635432712
ashok34589 <ashok34589@gmail.com>
2020-04-28 13:33:06 +0530

    [ashok]refactor. updated the ILabelsMAnager

--- ApplicationBusiness/Interfaces/ILabelManager.cs
+++ ApplicationBusiness/Interfaces/ILabelManager.cs
@@ -18,9 +18,9 @@ namespace ApplicationBusiness.Interfaces
     public interface ILabelManager
     {
         Task<int> AddLabel(LabelsModel model);
-        Task<int> DeleteLabel(int id);
+        Task<int> DeleteLabel(int id, string email);
         Task<int> UpdateLabel(LabelsModel model);
-        IEnumerable<LabelsModel> GetAllLabels();
-        LabelsModel GetLabel(int id);
+        IEnumerable<LabelsModel> GetAllLabels(string email);
+        LabelsModel GetLabel(int id, string email);
     }
 }
