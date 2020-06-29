0495ec3eb1d2afa56cb775ccf4e84340b7e71dae
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:14:55 +0530

    [ashok]add. added the IBookManager.cs file

---
+++ ApplicationBusiness/Interface/IBookManager.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IBookManager.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.ApplicationBusiness.Interface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using BookStore.Models;
+    using Microsoft.AspNetCore.Http;
+
+    /// <summary>
+    /// to Manager the books
+    /// </summary>
+    public interface IBookManager
+    {
+
+        /// <summary>
+        /// to add the new books to the Store
+        /// </summary>
+        /// <param name="file">Book Image</param>
+        /// <param name="book">Book information Model</param>
+        /// <returns>result</returns>
+        Task<int> AddBook(IFormFile file, Books book);
+
+        /// <summary>
+        /// to get the books available in the Store
+        /// </summary>
+        /// <returns>List of books</returns>
+        List<Books> GetBooks();
+    }
+}
