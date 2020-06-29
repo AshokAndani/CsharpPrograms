b8496ad2b7245bcbc26cbc08cf1f00ef3e4b0b1f
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:18:30 +0530

    [ashok]add. added the AddressTable.cs

---
+++ Models/AddressTable.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressTable.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// AddressTable
+    /// </summary>
+    public class AddressTable
+    {
+        [Key]
+        public Guid AddressID { get; set; }
+        public string AccountUserName { get; set; }
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string PinCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string LocalAddress { get; set; }
+    }
+}
