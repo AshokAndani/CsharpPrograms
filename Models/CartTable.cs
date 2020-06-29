517eb3ec112eafe8a7cb485d98204b4843c3857f
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:18:48 +0530

    [ashok]add. added the CartTable.cs

---
+++ Models/CartTable.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CartTable.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// CartTable
+    /// </summary>
+    public class CartTable
+    {
+        [Key]
+        public int ID { get; set; }
+        public Guid BookID { get; set; }
+        public string UserName { get; set; }
+    }
+}
