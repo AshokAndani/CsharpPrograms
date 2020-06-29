fea617d04bd3fc514238282ecc6553fc0ecd1a30
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:15:47 +0530

    [ashok]add. added the IBookStoreManager.cs file

---
+++ ApplicationBusiness/Interface/IBookStoreManager.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IBookStoreManager.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.ApplicationBusiness.Interface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using BookStore.Models;
+
+    /// <summary>
+    /// IBookStoreManager
+    /// </summary>
+    public interface IBookStoreManager
+    {
+        Task<int> CartBooks(Guid guid, string UserName);
+        List<Books> GetCartBooks(string UserName);
+        Task<int> AddToWishList(Guid guid, string UserName);
+        List<Books> GetWishList(string UserName);
+        Task<int> UpdateSummary(Guid BookID, Guid AddressID, string UserName);
+    }
+}
