53743d2b9152a697c7b8f138ffbd01e36957f1f8
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:21:09 +0530

    [ashok]add. added the WishList.cs

---
+++ Models/WishList.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WishList.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// WishList Model
+    /// </summary>
+    public class WishList
+    {
+        [Key]
+        public Guid WishListID { get; set; }
+        public string UserName { get; set; }
+        public Guid BookID { get; set; }
+    }
+}
