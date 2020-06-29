127f96f5521e228c37f072a01ded59e16da00b82
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:20:29 +0530

    [ashok]add. added the LoginViewModel.cs

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoginViewModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// LoginViewModel
+    /// </summary>
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
