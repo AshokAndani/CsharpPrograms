0d38a5a51e64529346f3c6b046c07d0f97058b43
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:19:48 +0530

    [ashok]add. added the Books.cs

---
+++ Models/Books.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Books.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Books Model
+    /// </summary>
+    public class Books
+    {
+        [Key]
+        public Guid BookId { get; set; }
+        public string BookName { get; set; }
+        public string ContentType { get; set; }
+        public string ImageName { get; set; }
+        public byte[] ImageContent { get; set; }
+        public int Price { get; set; }
+        public string Author { get; set; }
+        public int Stock { get; set; }
+    }
+}
