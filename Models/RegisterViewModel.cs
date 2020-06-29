4f53b75a1e4d743d4b53a34847f9bfa4d407f9ff
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:19:30 +0530

    [ashok]add. added the RegisterViewModel.cs

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RegisterViewModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// RegisterViewModel
+    /// </summary>
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+        public string  Password { get; set; }
+    }
+}
