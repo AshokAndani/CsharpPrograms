d89e6c6fe0ec03bd21ad64bb6d1efac3e10a83f1
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:20:50 +0530

    [ashok]add. added the SummayTable.cs

---
+++ Models/SummaryTable.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SummaryTable.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// SummaryTable
+    /// </summary>
+    public class SummaryTable
+    {
+        [Key]
+        public int ID { get; set; }
+        public string AccountUserName { get; set; }
+        public Guid BookID { get; set; }
+        public Guid AddressID { get; set; }
+    }
+}
