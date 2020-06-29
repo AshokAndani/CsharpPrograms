e034164fdf5d89b3033fbdcf93c0459a7d5aef90
ashok34589 <ashok34589@gmail.com>
2020-06-29 20:20:09 +0530

    [ashok]add. added the ChangePasswordModel.cs

---
+++ Models/ChangePasswordModel.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChangePasswordModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company
+// </copyright>
+// <creator Name="ASHOKKUMAR"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BookStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// ChangePasswordModel
+    /// </summary>
+    public class ChangePasswordModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, DataType(DataType.Password), Compare("Password", ErrorMessage = "Passwords does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
